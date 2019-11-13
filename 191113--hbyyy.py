fe4f154a1503fb9d3a66efdf01d2de6188533d15
hbyyy <qjaduddl94@gmail.com>
2019-11-13 18:22:01 +0900

    feat: 2016 문제 풀이
    해결했습니다

---
+++ 191113--hbyyy.py
@@ -0,0 +1,13 @@
+def solution(a, b):
+    week = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+    num_of_day_in_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day_of_year = 0
+    
+    # a월 b일까지 총 지난 일자를 계산
+    for i in range(0, a -1):
+        day_of_year += num_of_day_in_month[i]
+    
+    
+    day_of_year += b
+    answer = week[(day_of_year % 7) -1 ]
+    return answer
